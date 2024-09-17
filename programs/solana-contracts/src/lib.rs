97622544273067ec9bd9c09c09cd8498f730e0c6
Deepu <deepuharry02@gmail.com>
2024-09-17 22:08:58 +0530

    build successful

--- programs/solana-contracts/src/lib.rs
+++ programs/solana-contracts/src/lib.rs
@@ -1,15 +1,41 @@
 use anchor_lang::prelude::*;
+pub mod buy_node; 
 
 declare_id!("6kgSdKsaQGrWMVrCgp7RmXX7pnqVnDZ5JDJjTDvC2j62");
 
 #[program]
-pub mod solana_contracts {
+pub mod solana_contracts 
+{
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeContext>) -> Result<()> {
+        let owner_account = &mut ctx.accounts.owner_account; 
+        if owner_account.owner_pubkey == Pubkey::default() 
+        {
+            let caller = &ctx.accounts.caller.key();
+            owner_account.owner_pubkey = *caller;
+        }
+        else 
+        {
+            return Err(ProgramError::Custom(0).into());
+        }
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeContext<'info> 
+{
+    #[account(init, payer = caller, space = 8 + 32)]
+    pub owner_account: Account<'info, Owner>, 
+
+    #[account(mut)]
+    pub caller: Signer<'info>, 
+    pub system_program: Program<'info, System>, 
+}
+
+#[account]
+pub struct Owner 
+{
+    pub owner_pubkey: Pubkey, 
+}
