9c834dde7a67edad9ac1981320c0f00ca0170f0d
Deepu <deepuharry02@gmail.com>
2024-09-18 14:47:59 +0530

    shifted all the functions into lib.rs

--- tests/buy_node.t.ts
+++ tests/buy_node.t.ts
@@ -1,13 +1,13 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { SolanaContracts } from "../target/types/solana_contracts";
+import { BuyNodeContracts } from "../target/types/buy_node_contracts";
 
 describe("solana-contracts", () => {
 
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaContracts as Program<SolanaContracts>;
+  const program = anchor.workspace.SolanaContracts as Program<BuyNodeContracts>;
   let owner_account = anchor.web3.Keypair.generate();
   let buy_node_account = anchor.web3.Keypair.generate();
   let set_pda_account = anchor.web3.Keypair.generate();
@@ -16,21 +16,21 @@ describe("solana-contracts", () => {
   let funds_handler = anchor.web3.Keypair.generate();
 
   describe("Initialize:",()=>{
-    // it("Is initialized!", async () => {
-    //   const tx = await program.methods
-    //     .initialize()
-    //     .accounts({ 
-    //       ownerAccount: owner_account.publicKey,
-    //       caller: provider.publicKey,
-    //       systemProgram: anchor.web3.SystemProgram.programId,
-    //      })
-    //     .signers([owner_account])
-    //     .rpc();
-    //   console.log("Your transaction signature:", tx);
-    // });
+    it("Is initialized!", async () => {
+      const tx = await program.methods
+        .initialize()
+        .accounts({ 
+          ownerAccount: owner_account.publicKey,
+          caller: provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+         })
+        .signers([owner_account])
+        .rpc();
+      console.log("Your transaction signature:", tx);
+    });
     it("Should fail", async () => {
       const tx = await program.methods
-        .my_initialize()
+        .initialize()
         .accounts({ 
           ownerAccount: dummy.publicKey,
           caller: provider.publicKey,
@@ -45,7 +45,7 @@ describe("solana-contracts", () => {
   // describe("Setter and getter functions:",()=>{
   //   it("Should set and get funds handler:",async()=>{
   //     const tx = await program.methods
-  //       .set_funds_handler(funds_handler.publicKey)
+  //       .setFundsHandler(funds_handler.publicKey)
   //       .accounts({ 
   //         ownerAccount: dummy.publicKey,
   //         caller: provider.publicKey,
